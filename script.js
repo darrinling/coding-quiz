4d56073c300c7ebacace0eda916a7ba2f33ce3a8
darrinling <darrinling@gmail.com>
2022-03-19 15:59:15 -0600

    added high scores

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ var answer = document.querySelector("#answer");
 var highScores = document.querySelector("#highScores");
 var next = document.querySelector("#next");
 var endGame = document.querySelector("#endGame");
+var card = document.querySelector(".card");
+var playAgain = document.querySelector("#playAgain");
 
 var timer;
 var timerCount = 75;
@@ -26,7 +28,7 @@ var questions = [
   "Header", "Body", "Class", "ID"],
   ["Which of the following header sizes is the smallest?", "h6", "h7", "h8", "h9"]
 ];
-var player = 'Player 1';
+
 var score;
 
 function startTimer() {
@@ -132,17 +134,114 @@ function gameOver() {
   clearInterval(timer);
   answer.style.display="none";
   next.style.display="none";
-  quizElement.textContent = "GAME OVER - let's see how you did";
+  quizElement.textContent = "GAME OVER - LET'S SEE HOW YOU DID!";
   endGame.style.display="block";
 }
 
-next.addEventListener("click", nextQuiz);
-highScores.addEventListener("click", displayScores);
-endGame.addEventListener("click", displayScores);
-
-function displayScores() {
- alert('High Score: ' + player + ': ' + (score+10));
+function another() {
+  location.reload();
 }
 
+next.addEventListener("click", nextQuiz);
+highScores.addEventListener("click", showCard);
+endGame.addEventListener("click", showCard);
+playAgain.addEventListener("click", another);
 
+function showCard() {
+  clearInterval(timer);
+  quizElement.style.display="none";
+  startButton.style.display="none";
+  endGame.style.display="none";
+  card.style.display="block";
+  playAgain.style.display="block";
+}
+
+var todoInput = document.querySelector("#todo-text");
+var todoForm = document.querySelector("#todo-form");
+var todoList = document.querySelector("#todo-list");
+var todoCountSpan = document.querySelector("#todo-count");
+
+var todos = [];
+
+// The following function renders items in a todo list as <li> elements
+function renderTodos() {
+  // Clear todoList element and update todoCountSpan
+  todoList.innerHTML = "";
+  todoCountSpan.textContent = todos.length;
+
+  // Render a new li for each todo
+  for (var i = 0; i < todos.length; i++) {
+    var points = score;
+    var todo = todos[i] + ': ' + (points+10);
+
+    var li = document.createElement("li");
+    li.textContent = todo;
+    li.setAttribute("data-index", i);
+
+    var button = document.createElement("button");
+    button.style.margin="10px";
+    button.textContent = 'Remove High Score';
+
+    li.appendChild(button);
+    todoList.appendChild(li);
+  }
+}
+
+// This function is being called below and will run when the page loads.
+function init() {
+  // Get stored todos from localStorage
+  var storedTodos = JSON.parse(localStorage.getItem("todos"));
+
+  // If todos were retrieved from localStorage, update the todos array to it
+  if (storedTodos !== null) {
+    todos = storedTodos;
+  }
+
+  // This is a helper function that will render todos to the DOM
+  renderTodos();
+}
+
+function storeTodos() {
+  // Stringify and set key in localStorage to todos array
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// Add submit event to form
+todoForm.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  var todoText = todoInput.value.trim();
+
+  // Return from function early if submitted todoText is blank
+  if (todoText === "") {
+    return;
+  }
+
+  // Add new todoText to todos array, clear the input
+  todos.push(todoText);
+  // todoInput.value = "";
+
+  // Store updated todos in localStorage, re-render the list
+  storeTodos();
+  renderTodos();
+});
+
+// Add click event to todoList element
+todoList.addEventListener("click", function(event) {
+  var element = event.target;
+
+  // Checks if element is a button
+  if (element.matches("button") === true) {
+    // Get its data-index value and remove the todo element from the list
+    var index = element.parentElement.getAttribute("data-index");
+    todos.splice(index, 1);
+
+    // Store updated todos in localStorage, re-render the list
+    storeTodos();
+    renderTodos();
+  }
+});
+
+// Calls init to retrieve data and render it to the page on load
+init();
 
